709bda88f2cad47d9f7dc158afbdb921d338770e
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-10-01 19:18:09 +0300

    Add password hashing before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 
+import bcrpt from "bcrypt";
+
 const userSchema = new Schema({
     email: {
         type: String,
@@ -9,6 +11,12 @@ const userSchema = new Schema({
         type: String,
         require: true
     }
+});
+
+userSchema.pre("save", async function() {
+    // const salt = await bcrpt.genSalt(12);
+    this.password = await bcrpt.hash(this.password, 12);
+
 });
 
 const User =  model("User", userSchema);
